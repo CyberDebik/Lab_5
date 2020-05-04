1be9f437b1754a60d1ec08d3897fa29ea08dd6e2
Cyber.Debik <cyber.debik@gmail.com>
2020-05-04 15:35:25 +0300

    update

--- App/CSV.java
+++ App/CSV.java
@@ -18,54 +18,54 @@ public class CSV {
         File file = new File(file_path);
         try {
             Scanner reader = new Scanner(file);
-                Scanner scanner;
-                String line;
-                int index = 0;
-                String name = null;
-                String x = null;
-                String y = null;
-                ZonedDateTime date = null;
-                String enginePower = null;
-                String numberOfWheels = null;
-                VehicleType type = null;
-                FuelType fuelType = null;
-                while (reader.hasNextLine()) {
-                    line = reader.nextLine();
-                    scanner = new Scanner(line);
-                    scanner.useDelimiter(",");
-                    try {
-                        while (scanner.hasNext()) {
-                            String info_from_file = scanner.next();
-                            if (index == 0) {
-                                name = info_from_file;
-                            } else if (index == 1) {
-                                x = info_from_file;
-                            } else if (index == 2) {
-                                y = info_from_file;
-                            } else if (index == 3) {
-                                date = ZonedDateTime.parse(info_from_file);
-                            } else if (index == 4) {
-                                enginePower = info_from_file;
-                            } else if (index == 5) {
-                                numberOfWheels = info_from_file;
-                            } else if (index == 6) {
-                                if (info_from_file.equals("PLANE") || info_from_file.equals("DRONE") || info_from_file.equals("BICYCLE") || info_from_file.equals("MOTORCYCLE"))
-                                    type = VehicleType.valueOf(info_from_file);
-                                else if (info_from_file.equals("null") || info_from_file.trim().equals(""))
-                                    type = null;
-                            } else if (index == 7) {
-                                fuelType = FuelType.valueOf(info_from_file);
-                            }
-                            index++;
+            Scanner scanner;
+            String line;
+            int index = 0;
+            String name = null;
+            String x = null;
+            String y = null;
+            ZonedDateTime date = null;
+            String enginePower = null;
+            String numberOfWheels = null;
+            VehicleType type = null;
+            FuelType fuelType = null;
+            while (reader.hasNextLine()) {
+                line = reader.nextLine();
+                scanner = new Scanner(line);
+                scanner.useDelimiter(",");
+                try {
+                    while (scanner.hasNext()) {
+                        String info_from_file = scanner.next();
+                        if (index == 0) {
+                            name = info_from_file;
+                        } else if (index == 1) {
+                            x = info_from_file;
+                        } else if (index == 2) {
+                            y = info_from_file;
+                        } else if (index == 3) {
+                            date = ZonedDateTime.parse(info_from_file);
+                        } else if (index == 4) {
+                            enginePower = info_from_file;
+                        } else if (index == 5) {
+                            numberOfWheels = info_from_file;
+                        } else if (index == 6) {
+                            if (info_from_file.equals("PLANE") || info_from_file.equals("DRONE") || info_from_file.equals("BICYCLE") || info_from_file.equals("MOTORCYCLE"))
+                                type = VehicleType.valueOf(info_from_file);
+                            else if (info_from_file.equals("null") || info_from_file.trim().equals(""))
+                                type = null;
+                        } else if (index == 7) {
+                            fuelType = FuelType.valueOf(info_from_file);
                         }
-                    } catch (Exception e) {
-                        System.out.println("Ошибка, неверный формат данных");
-                        return;
+                        index++;
                     }
-                    vehicleList.vehicles.add(new Vehicle(name, x, y, date, enginePower, numberOfWheels, type, fuelType));
-                    index = 0;
+                } catch (Exception e) {
+                    System.out.println("Ошибка, неверный формат данных");
+                    return;
                 }
-                reader.close();
+                vehicleList.vehicles.add(new Vehicle(name, x, y, date, enginePower, numberOfWheels, type, fuelType));
+                index = 0;
+            }
+            reader.close();
         } catch (Exception e) {
             System.out.println("Файл с коллекцией прочитать нельзя");
         }
